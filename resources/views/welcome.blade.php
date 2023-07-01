1285fc9cf7325eb1db7a2e9be542eb78067ce03c
csquychdi2k.0111@gmail.com <csquychdi2k.0111@gmail.com>
2023-07-02 00:59:45 +0700

    2 vie

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,6 +21,16 @@
         </style>
     </head>
     <body class="antialiased">
+            <?php
+                try {
+                    \DB::connection()->getPDO();
+                    echo \DB::connection()->getDatabaseName();
+                    } catch (\Exception $e) {
+                    echo 'None';
+                }
+
+                
+            ?>
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
